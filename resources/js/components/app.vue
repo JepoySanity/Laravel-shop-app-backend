ddcb11167c83633b4ed46ab92843c0e471b1be6b
Jepoy Factoran <factoran20@gmail.com>
2022-07-11 22:38:01 +0800

    added products fetch from api to UI

--- resources/js/components/app.vue
+++ resources/js/components/app.vue
@@ -1,107 +1,37 @@
 <template lang="">
     <div>
         <navbar></navbar>
-        <div class="container">
-            <div class="row mt-4">
-                <div class="col-lg-3 mb-4">
-                    <div class="card" style="width: 18rem">
-                        <img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=446&hei=410&fmt=jpeg&qlt=95&.v=1633027804000"
-                            class="card-img-top"
-                            alt="..."
-                        />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card
-                                title and make up the bulk of the card's
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card" style="width: 18rem">
-                        <img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=446&hei=410&fmt=jpeg&qlt=95&.v=1633027804000"
-                            class="card-img-top"
-                            alt="..."
-                        />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card
-                                title and make up the bulk of the card's
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card" style="width: 18rem">
-                        <img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=446&hei=410&fmt=jpeg&qlt=95&.v=1633027804000"
-                            class="card-img-top"
-                            alt="..."
-                        />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card
-                                title and make up the bulk of the card's
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card" style="width: 18rem">
-                        <img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=446&hei=410&fmt=jpeg&qlt=95&.v=1633027804000"
-                            class="card-img-top"
-                            alt="..."
-                        />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card
-                                title and make up the bulk of the card's
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 mb-4">
-                    <div class="card" style="width: 18rem">
-                        <img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=446&hei=410&fmt=jpeg&qlt=95&.v=1633027804000"
-                            class="card-img-top"
-                            alt="..."
-                        />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">
-                                Some quick example text to build on the card
-                                title and make up the bulk of the card's
-                                content.
-                            </p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <products :products="products" />
     </div>
 </template>
 <script>
+import axios from "axios";
 import navbar from "./layout/navbar";
+import products from "./products";
 export default {
+    data: function () {
+        return {
+            products: [],
+        };
+    },
     components: {
         navbar,
+        products,
+    },
+    methods: {
+        getProducts() {
+            axios
+                .get("api/products")
+                .then((response) => {
+                    this.products = response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+    },
+    created() {
+        this.getProducts();
     },
 };
 </script>
